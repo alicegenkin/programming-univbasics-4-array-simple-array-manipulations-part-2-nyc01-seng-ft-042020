abd5916462172e6af0e412810f833cf304fdd93a
Alice <agenkin1@gmail.com>
2020-03-31 22:19:05 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array1, array2)
+  return array1.concat(array2)
+end
+
+  def using_insert(array1, elem)
+    return array1.insert(4,elem)
+  end
+
+  def using_uniq(array)
+    return array.uniq
+  end
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array,str)
+  return array.delete(str)
+end
+
+def using_delete_at (array,integer)
+  return array.delete_at(integer)
+end
